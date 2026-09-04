be51c7f6ca1df18cf259024c559afaadb0fbce83
118kyo2026zai-code <118kyo2026zai@gmail.com>
2026-09-05 07:50:22 +0900

    Create date.js

---
+++ date.js
@@ -0,0 +1,158 @@
+const data = [
+
+/* 肯定文第三文型 */
+
+{unit:"肯定文第三文型",ja:"僕は自転車が欲しい",en:"I want a bike."},
+
+{unit:"肯定文第三文型",ja:"私は英語を教える",en:"I teach English."},
+
+{unit:"肯定文第三文型",ja:"あなたは宿題をする",en:"You do your homework."},
+
+{unit:"肯定文第三文型",ja:"私たちはあなたの友だちを助ける",en:"We help your friend."},
+
+{unit:"肯定文第三文型",ja:"私たちは英語を勉強する",en:"we study English."},
+
+{unit:"肯定文第三文型",ja:"あなたは顔を洗う",en:"You wash your face."},
+
+{unit:"肯定文第三文型",ja:"私はあなたの名前を書く",en:"I write your name."},
+
+{unit:"肯定文第三文型",ja:"僕はコンピューターを使う",en:"I use a computer."},
+
+{unit:"肯定文第三文型",ja:"僕は英語を話す",en:"I speak English."},
+
+{unit:"肯定文第三文型",ja:"私たちは夕食を作る",en:"We cook denner."},
+
+{unit:"肯定文第三文型",ja:"僕はダンスが好きです",en:"I like dance."},
+
+{unit:"肯定文第三文型",ja:"あなたたちは寿司を作る",en:"You make sushi."},
+
+{unit:"肯定文第三文型",ja:"僕たちは野球をする",en:"We play baseball."},
+
+{unit:"肯定文第三文型",ja:"私はお茶を飲む",en:"I drink tea."},
+
+{unit:"肯定文第三文型",ja:"私はそのドアを開ける",en:"I open the door."},
+
+{unit:"肯定文第三文型",ja:"君は朝食を食べる",en:"You eat breakfast."},
+
+{unit:"肯定文第三文型",ja:"あなたはあなたの部屋を掃除する",en:"You clean your room."},
+
+{unit:"肯定文第三文型",ja:"私はあなたの鉛筆を持ち上げる",en:"I pick up your pencil."},
+    
+{unit:"肯定文第三文型",ja:"私は私の鉛筆を置く",en:"I put down my pencil."},
+    
+{unit:"肯定文第三文型",ja:"私は京都を訪れる",en:"I visit Kyoto."},
+    
+{unit:"肯定文第三文型",ja:"あなたはあなたの滞在を楽しみます",en:"You enjoy your stay."},
+    
+{unit:"肯定文第三文型",ja:"私は一冊の本を読みます",en:"I read a book."},
+
+
+/* 疑問文第三文型 */
+
+{unit:"疑問文第三文型",ja:"あなたは自転車が欲しいですか？",en:"Do you want a bike?"},
+
+{unit:"疑問文第三文型",ja:"あなたは英語を教えますか？",en:"Do you teach English?"},
+
+{unit:"疑問文第三文型",ja:"あなたは宿題をしますか？",en:"Do yo do homework?"},
+
+{unit:"疑問文第三文型",ja:"私たちはあなたの友だちを助けますか？",en:"Do we help your friend?"},
+
+{unit:"疑問文第三文型",ja:"あなたは算数を勉強しますか",en:"Do you study math?"},
+
+{unit:"疑問文第三文型",ja:"あなたは顔を洗いますか",en:"Do you wash your face?"},
+
+{unit:"疑問文第三文型",ja:"私はあなたの名前を書きますか？",en:"Do I write your name?"},
+
+{unit:"疑問文第三文型",ja:"あなたはコンピュータを使いますか？",en:"Do you use a computer?"},
+
+{unit:"疑問文第三文型",ja:"あなたは英語を話しますか？",en:"Do you speak English?"},
+
+{unit:"疑問文第三文型",ja:"あなたたちは夕食を作りますか？",en:"Do you cook dinner?"},
+
+{unit:"疑問文第三文型",ja:"君はダンスが好きですか？",en:"Do you like dance?"},
+
+{unit:"疑問文第三文型",ja:"あなたは寿司を作りますか",en:"Do you make sushi?"},
+
+{unit:"疑問文第三文型",ja:"私たちは野球をしますか",en:"Do we play baseball?"},
+
+{unit:"疑問文第三文型",ja:"あなたはお茶を飲みますか",en:"Do you drink tea?"},
+
+{unit:"疑問文第三文型",ja:"私はそのドアを開けますか",en:"Do we open the door?"},    
+
+{unit:"疑問文第三文型",ja:"あなたは朝食を食べますか",en:"Do you eat breakfast?"}, 
+
+{unit:"疑問文第三文型",ja:"あなたはあなたの部屋を掃除しますか",en:"Do you clean your room?"}, 
+
+{unit:"疑問文第三文型",ja:"あなたはあなたの鉛筆を持ち上げますか？",en:"Do you pick up your pencil?"},
+
+{unit:"疑問文第三文型",ja:"あなたはあなたの鉛筆を置きますか？",en:"Do you put down your pencil?"},
+
+{unit:"疑問文第三文型",ja:"あなたは京都を訪れますか？",en:"Do you visit Kyoto?"},
+
+{unit:"疑問文第三文型",ja:"あなたはあなたの滞在を楽しみますか？",en:"Do you enjoy your stay?"},
+
+{unit:"疑問文第三文型",ja:"あなたは本を一冊読みますか？",en:"Do you read a book?"},
+
+
+    /* 命令文第三文型 */
+
+{unit:"命令文第三文型",ja:"宿題をしなさい",en:"Do your homework."},
+
+{unit:"命令文第三文型",ja:"あなたの友だちを助けなさい",en:"Help your friend."},
+
+{unit:"命令文第三文型",ja:"英語を勉強しなさい",en:"Study English."},
+
+{unit:"命令文第三文型",ja:"顔を洗いなさい",en:"Wash your face."},
+
+{unit:"命令文第三文型",ja:"あなたの名前を書きなさい",en:"Write your name."},
+
+{unit:"命令文第三文型",ja:"コンピューターを使いなさい",en:"Use a computer."},
+
+{unit:"命令文第三文型",ja:"英語を話しなさい",en:"Speak English."},
+
+{unit:"命令文第三文型",ja:"夕食を作りなさい",en:"Cook dinner."},
+
+{unit:"命令文第三文型",ja:"寿司を作りなさい",en:"Make sushi."},
+
+{unit:"命令文第三文型",ja:"野球をしなさい",en:"Play baseball."},
+
+{unit:"命令文第三文型",ja:"お茶を飲みなさい",en:"Drink tea."},
+
+{unit:"命令文第三文型",ja:"そのドアを開けなさい",en:"Open the door."},
+
+{unit:"命令文第三文型",ja:"朝食を食べなさい",en:"Eat breakfast."},
+
+{unit:"命令文第三文型",ja:"あなたの部屋を掃除しなさい",en:"Clean your room."},
+
+{unit:"命令文第三文型",ja:"あなたの鉛筆を持ち上げなさい",en:"Pick up your pencil."},
+
+{unit:"命令文第三文型",ja:"あなたの鉛筆を置きなさい",en:"Put down your pencil."},
+
+{unit:"命令文第三文型",ja:"京都を訪れなさい",en:"Visit Kyoto."},
+
+{unit:"命令文第三文型",ja:"あなたの滞在を楽しみなさい",en:"Enjoy your stay."},
+
+{unit:"命令文第三文型",ja:"本を一冊読みなさい",en:"Read a book."},
+
+    /*疑問詞③*/
+    {unit:"疑問詞③", ja:"あなたはどの教科が好きですか。", en:"What subject do you like?"},
+{unit:"疑問詞③", ja:"あなたは何歳ですか。12才です。", en:"How old are you? I'm twelve."},
+{unit:"疑問詞③", ja:"あなたは普段、放課後にどんなスポーツをしますか。", en:"What sports do you usually do after school?"},
+{unit:"疑問詞③", ja:"けんの身長はいくつですか。彼は160cmです。", en:"How tall is Ken? He is 160 centimeters tall."},
+{unit:"疑問詞③", ja:"この橋はどのくらいの長さですか。それは10メートルです。", en:"How long is this bridge? It's ten meters long."},
+{unit:"疑問詞③", ja:"どの本が面白いですか。", en:"Which book is interesting?"},
+{unit:"疑問詞③", ja:"トイレはどこにありますか。", en:"Where is the restroom?"},
+{unit:"疑問詞③", ja:"今日は何曜日ですか。木曜日です。", en:"What day is it today? It's Thursday."},
+{unit:"疑問詞③", ja:"何故、彼は忙しいのですか。", en:"Why is he busy?"},
+{unit:"疑問詞③", ja:"家では英語と日本語、どちらを話しますか。", en:"Which do you speak at home, English or Japanese?"},
+{unit:"疑問詞③", ja:"彼はどうやってそこに行きますか。", en:"How does he go there?"},
+{unit:"疑問詞③", ja:"彼はバスでそこへ行きます。", en:"He goes there by bus."},
+{unit:"疑問詞③", ja:"彼女はどんな音楽が好きですか。", en:"What kind of music does she like?"},
+{unit:"疑問詞③", ja:"授業はいつ始まりますか。9時です。", en:"When does the class begin? At nine."},
+{unit:"疑問詞③", ja:"箱の中に何がありますか。", en:"What is in the box?"},
+{unit:"疑問詞③", ja:"誰が速く走りますか。", en:"Who runs fast?"}
+,
+  ];
+
+
+
